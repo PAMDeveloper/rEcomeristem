8b131344e59c9d3f62b36ad8058b374e4314ad70
gbeurier <beurier@cirad.fr>
2018-09-04 15:52:34 +0200

    dbg fix

--- src/utils/ParametersReader.hpp
+++ src/utils/ParametersReader.hpp
@@ -32,11 +32,11 @@ vector<string> split(const std::string &line, char delim) {
 }
 
 
-inline std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
+//inline std::vector<std::string> split(const std::string &s, char delim) {
+//    std::vector<std::string> elems;
+//    split(s, delim, std::back_inserter(elems));
+//    return elems;
+//}
 
 
 inline double round( double val, int decimal )
