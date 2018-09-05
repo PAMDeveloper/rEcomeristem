dc5e65ca7665601bcf3ac802b7d3ebd3109da774
gbeurier <beurier@cirad.fr>
2018-09-05 10:22:26 +0200

    dbg fix

--- src/utils/ParametersReader.hpp
+++ src/utils/ParametersReader.hpp
@@ -132,7 +132,41 @@ public:
 
     }
 
+
 #ifdef UNSAFE_RUN
+    std::istream& safeGetline(std::istream& is, std::string& t)
+    {
+        t.clear();
+
+        // The characters in the stream are read one-by-one using a std::streambuf.
+        // That is faster than reading them one-by-one using the std::istream.
+        // Code that uses streambuf this way must be guarded by a sentry object.
+        // The sentry object performs various tasks,
+        // such as thread synchronization and updating the stream state.
+
+        std::istream::sentry se(is, true);
+        std::streambuf* sb = is.rdbuf();
+
+        for(;;) {
+            int c = sb->sbumpc();
+            switch (c) {
+            case '\n':
+                return is;
+            case '\r':
+                if(sb->sgetc() == '\n')
+                    sb->sbumpc();
+                return is;
+            case std::streambuf::traits_type::eof():
+                // Also handle the case when the last line has no line ending
+                if(t.empty())
+                    is.setstate(std::ios::eofbit);
+                return is;
+            default:
+                t += (char)c;
+            }
+        }
+    }
+
     map<string, vector<double>> loadCleanObsFromFile(const std::string &file_path, const SimpleView & view) {
         std::ifstream vObsFile(file_path);
         std::string line;
@@ -152,7 +186,7 @@ public:
             delete s;
         }
 
-        while (std::getline(vObsFile, line))
+        while (safeGetline(vObsFile, line))
         {
             std::cout << line << "\n";
             line.erase (line.begin(), line.end()-2);
